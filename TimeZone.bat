da83cbaddcc73963d96448f227223494ac714868
Ben <44791946+BenJaminL11@users.noreply.github.com>
2022-01-20 13:09:35 +1000

    Update TimeZone.bat

--- TimeZone.bat
+++ TimeZone.bat
@@ -1,3 +1,5 @@
 @echo off
+TITLE: Change Time Zone to AEST AEST UTC +10:00 Brisbane
 tzutil /s "E. Australia Standard Time"
-tzutil /s "E. Australia Standard Time Time_dstoff"
+ECHO Time Zone Has Been Changed To AEST UTC +10:00 Brisbane
+PAUSE
